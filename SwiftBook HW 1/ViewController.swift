436320ce2fecbebf8e15188cb308d607553e1c58
lvaphotographer <lvaphotographer@gmail.com>
2022-06-15 15:51:43 +0300

    Initial Commit

---
+++ SwiftBook HW 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBook HW 1
+//
+//  Created by Владимир Лютов on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
